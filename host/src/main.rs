14392d344447544471182ae7bc84615b41043010
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-10-23 18:47:00 +0100

    Hello World

---
+++ host/src/main.rs
@@ -0,0 +1,38 @@
+// TODO: Update the name of the method loaded by the prover. E.g., if the method
+// is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
+// `METHOD_NAME_ID` with `MULTIPLY_ID`
+use hello_world_methods::{MULTIPLY_ELF, MULTIPLY_ID};
+use risc0_zkvm::{default_prover, serde::{from_slice, to_vec}, ExecutorEnv};
+
+fn main() {
+    let a: u64 = 17;
+    let b: u64 = 23;
+
+    // First, we construct an executor environment
+    //let env = ExecutorEnv::builder().build().unwrap();
+
+    // First, we construct an executor environment
+    let env = ExecutorEnv::builder()
+        // Send a & b to the guest
+        .add_input(&to_vec(&a).unwrap())
+        .add_input(&to_vec(&b).unwrap())
+        .build()
+        .unwrap();
+
+    // For example:
+    // let env = ExecutorEnv::builder().add_input(&vec).build().unwrap();
+
+    // Obtain the default prover.
+    let prover = default_prover();
+
+    // Produce a receipt by proving the specified ELF binary.
+    let receipt = prover.prove_elf(env, MULTIPLY_ELF).unwrap();
+
+    receipt.verify(MULTIPLY_ID).unwrap();
+
+    // Extract journal of receipt (i.e. output c, where c = a * b)
+    let c: u64 = from_slice(&receipt.journal).unwrap();
+
+    // Print an assertion
+    println!("Hello, world! I know the factors of {}, and I can prove it!", c);
+}
