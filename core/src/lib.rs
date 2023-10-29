b1d721acf96b7605e1d385f0a789d49355fb6dcc
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-10-29 10:54:52 +0000

    cleanup

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -12,27 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use risc0_zkvm::sha::Digest;
 use serde::{Deserialize, Serialize};
-
-pub const WORD_LENGTH: usize = 5;
-
-#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
-pub enum LetterFeedback {
-    Correct,
-    Present,
-    #[default]
-    Miss,
-}
-
-#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
-pub struct WordFeedback(pub [LetterFeedback; WORD_LENGTH]);
-
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct GameState {
-    pub correct_word_hash: Digest,
-    pub feedback: WordFeedback,
-}
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Balances {
@@ -68,31 +48,5 @@ impl Policy for AmountLimitPolicy {
 impl Policy for SenderWhitelistPolicy {
     fn is_valid(&self, transaction: Transaction) -> bool {
         self.whitelisted_senders.contains(&transaction.from)
-    }
-}
-
-
-
-
-
-
-impl WordFeedback {
-    pub fn game_is_won(&self) -> bool {
-        self.0.iter().all(|x| *x == LetterFeedback::Correct)
-    }
-
-    #[cfg(not(target_os = "zkvm"))]
-    pub fn print(&self, guess_word: &str) {
-        print!("Your results: ");
-        for i in 0..WORD_LENGTH {
-            match self.0[i] {
-                LetterFeedback::Correct => print!("\x1b[41m"), // green
-                LetterFeedback::Present => print!("\x1b[43m"), // yellow
-                LetterFeedback::Miss => print!("\x1b[40m"),    // black
-            }
-            print!("{:}", guess_word.chars().nth(i).unwrap());
-        }
-        println!("\x1b[0m");
-        println!();
     }
 }
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -15,8 +15,7 @@
 #![no_main]
 
 use risc0_zkvm::guest::env;
-use risc0_zkvm::sha::{Impl, Sha256};
-use wordle_core::{Balances, Transaction, AmountLimitPolicy, SenderWhitelistPolicy, Policy};
+use zkauth_core::{Balances, Transaction, AmountLimitPolicy, SenderWhitelistPolicy, Policy};
 
 risc0_zkvm::guest::entry!(main);
 
@@ -27,48 +26,6 @@ pub fn main() {
 
     check_policies(&tx);
     
-
-    // assert_eq!(
-    //     secret.chars().count(),
-    //     WORD_LENGTH,
-    //     "secret must have length 5!"
-    // );
-
-    // assert_eq!(
-    //     guess.chars().count(),
-    //     WORD_LENGTH,
-    //     "guess must have length 5!"
-    // );
-
-   // let mut feedback: WordFeedback = WordFeedback::default();
-
-    // to avoid false positive partial matches, create a pool of only letters
-    // that didn't have an exact match
-    // let mut secret_unmatched: String = String::from("");
-
-    // for i in 0..WORD_LENGTH {
-    //     if secret.as_bytes()[i] != guess.as_bytes()[i] {
-    //         secret_unmatched.push(secret.as_bytes()[i] as char);
-    //    }
-    // }
-
-    // // second round for distinguishing partial matches from misses
-    // for i in 0..WORD_LENGTH {
-    //     feedback.0[i] = if secret.as_bytes()[i] == guess.as_bytes()[i] {
-    //         LetterFeedback::Correct
-    //     } else if secret_unmatched.as_bytes().contains(&guess.as_bytes()[i]) {
-    //         LetterFeedback::Present
-    //     } else {
-    //         LetterFeedback::Miss
-    //     }
-    // }
-
-    // let correct_word_hash = *Impl::hash_bytes(&secret.as_bytes());
-    // let game_state = GameState {
-    //     correct_word_hash,
-    //     feedback,
-    // };
-
     if tx.from == 0 {
         if tx.to == 1 {
             _balance_user0 -= tx.amount;
@@ -81,13 +38,11 @@ pub fn main() {
             _balance_user0 += tx.amount;
         }
     }
-    println!("aa {0} bb {1}", _balance_user0, _balance_user1);
 
     let balances = Balances {
         balance_user0: _balance_user0,
         balance_user1: _balance_user1
-    };
-    
+    };    
 
     env::commit(&balances);
 }
